442b0dcf40d92a4ad5e0928fd5a5f4359f9e23e1
Loyoenock <loyoenock@gmail.com>
2023-12-12 21:58:02 +0300

    Recover password

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import { Gutter } from '../../_components/Gutter'
 import { RenderParams } from '../../_components/RenderParams'
 import { getMeUser } from '../../_utilities/getMeUser'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
@@ -17,11 +18,33 @@ export default async function CreateAccount() {
   })
 
   return (
-    <Gutter className={classes.createAccount}>
-      <h1>Create Account</h1>
-      <RenderParams />
-      <CreateAccountForm />
-    </Gutter>
+    <section className={classes.createAccount}>
+      <div className={classes.heroImage}>
+        <Link href="/">
+          <Image
+            src="/logo-header-black.svg"
+            alt="logo"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.wrapForm}>
+        <div className={classes.formBlock}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Create Account</h3>
+            <Image src="/assets/icons/greetings.svg" alt="logo" width={30} height={30} />
+          </div>
+          <p> Enter your details </p>
+
+          <CreateAccountForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
