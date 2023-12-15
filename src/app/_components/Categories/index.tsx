1f483a77bcdf521fd0a810a934f09dbed1d9b81f
Loyoenock <loyoenock@gmail.com>
2023-12-15 11:59:01 +0300

    product categories

---
+++ src/app/_components/Categories/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+import classes from './index.module.scss'
+
+const Categories = () => {
+  return (
+    <section className={classes.container}>
+      <div className={classes.wrapTitle}>
+        <h3>Shop by Categories </h3>
+        <Link href="/products"> Show All </Link>
+      </div>
+    </section>
+  )
+}
+
+export default Categories
