85472ee1e7d5a7662fed37ad85f9488213afd470
Loyoenock <loyoenock@gmail.com>
2023-12-20 08:39:03 +0300

    Cart page

--- src/app/_components/RemoveFromCartButton/index.tsx
+++ src/app/_components/RemoveFromCartButton/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 import { Product } from '../../../payload/payload-types'
 import { useCart } from '../../_providers/Cart'
@@ -27,7 +28,13 @@ export const RemoveFromCartButton: React.FC<{
       }}
       className={[className, classes.removeFromCartButton].filter(Boolean).join(' ')}
     >
-      Remove
+      <Image
+        src="/assets/icons/trash.svg"
+        alt="trash"
+        width={16}
+        height={16}
+        className={classes.qtnBt}
+      />
     </button>
   )
 }
