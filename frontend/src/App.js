83cc43e273c7d979d8866f54dab649b5a6bdad2e
Loyoenock <loyoenock@gmail.com>
2024-06-11 17:02:46 +0300

    Routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup'; // Import the LoginSignup component
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/ladies" element={<ShopCategory category="ladies" />} />      
+        <Route path="/kids apparel" element={<ShopCategory category="kids apparel" />} />
+        <Route path="/scents & decor" element={<ShopCategory category="scents & decor" />} />
+        <Route path="/accessories" element={<ShopCategory category="accessories" />} />
+        <Route path="/product" element={<Product />}>
+          <Route path=":productId" element={<Product />} />
+        </Route>
+        <Route path="cart" element={<Cart />} />
+        <Route path="login" element={<LoginSignup />} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop")
@@ -11,15 +12,16 @@ const Navbar = () => {
         <img src={logo} alt='logo' height={120} />        
       </div>
       <ul className='nav-links'>
-        <li onClick={() => {setMenu("shop")}}>Shop {menu === "shop" ? <hr /> : <></>}</li>
-        <li onClick={() => {setMenu("ladies")}}>Ladies {menu === "ladies" ? <hr /> : <></>}</li>
-        <li onClick={() => {setMenu("kids apperal")}}>Kids Apperal {menu === "kids apperal" ? <hr /> : <></>}</li>
-        <li onClick={() => {setMenu("scents & decor")}}>Scents & Decor  {menu === "scents & decor" ? <hr /> : <></>}</li>
-        <li onClick={() => {setMenu("accessoories")}}>Accessories  {menu === "accessoories" ? <hr /> : <></>}</li>   
+        <li onClick={() => {setMenu("shop")}}> <Link to="/" style={{textDecoration: "none"}}> Shop </Link> {menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("ladies")}}> <Link to="/ladies" style={{textDecoration: "none"}}> Ladies </Link> {menu === "ladies" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("kids apperal")}}><Link to="/kids apperal" style={{textDecoration: "none"}}>Kids Apperal</Link> {menu === "kids apperal" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("scents & decor")}}><Link to="/scents & decor">Scents & Decor</Link>{menu === "scents & decor" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("accessoories")}}><Link to="/accessoories">Accessories </Link> {menu === "accessoories" ? <hr /> : <></>}</li>   
       </ul>
       <div className='nav-login-cart'>
-        <button>Login</button>
-        <img src={cart_icon} alt='cart' height={50} />
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/cart"    ><img src={cart_icon} alt='cart' height={50} /></Link>
+        
         <div className='nav-cart-count'>0</div>
       </div>
     </div>
