ce77bd5c3c456e7f0c663fd178d9f3957f91877e
Loyoenock <loyoenock@gmail.com>
2024-06-11 15:36:09 +0300

    Menu state management implemented for active menu

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop")
   return (
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt='logo' height={120} />        
       </div>
       <ul className='nav-links'>
-        <li>Shop <hr /></li>
-        <li>Ladies</li>
-        <li>Kids Apperal</li>
-        <li>Home & Decor</li>
-        <li>Accessories</li>
+        <li onClick={() => {setMenu("shop")}}>Shop {menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("ladies")}}>Ladies {menu === "ladies" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("kids apperal")}}>Kids Apperal {menu === "kids apperal" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("scents & decor")}}>Scents & Decor  {menu === "scents & decor" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("accessoories")}}>Accessories  {menu === "accessoories" ? <hr /> : <></>}</li>   
       </ul>
       <div className='nav-login-cart'>
         <button>Login</button>
