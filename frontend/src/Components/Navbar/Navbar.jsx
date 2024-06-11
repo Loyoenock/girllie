012b0601767b51bf0cee305f8a0eb4737c4e5ecb
Loyoenock <loyoenock@gmail.com>
2024-06-11 09:39:05 +0300

    Navbar - styles applied

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -7,11 +7,10 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-        <img src={logo} alt='logo' height={150} />
-        <p>Your Style Secret</p>
+        <img src={logo} alt='logo' height={120} />        
       </div>
       <ul className='nav-links'>
-        <li>Shop</li>
+        <li>Shop <hr /></li>
         <li>Ladies</li>
         <li>Kids Apperal</li>
         <li>Home & Decor</li>
@@ -20,6 +19,7 @@ const Navbar = () => {
       <div className='nav-login-cart'>
         <button>Login</button>
         <img src={cart_icon} alt='cart' height={50} />
+        <div className='nav-cart-count'>0</div>
       </div>
     </div>
   )
