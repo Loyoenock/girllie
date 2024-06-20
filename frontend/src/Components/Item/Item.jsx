6826f81f5795dee459b283305a8dcdddcd26712d
Loyoenock <loyoenock@gmail.com>
2024-06-20 15:28:18 +0300

    Component - Related product added

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Item = ({id, image, name, new_price, old_price }) => {
   return (
     <div className='item'>
-       <Link to={`/product/${id}`}> <img src={image} alt='' width={250} /> </Link>
+       <Link to={`/product/${id}`}> <img onClick={window.scrollTo(0,0)} src={image} alt='' width={250} /> </Link>
         <p>{name}</p>
         <div className='item-prices'>
             <div className='item_price-new'>
